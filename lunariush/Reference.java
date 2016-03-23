75fe80621713ab344758fc9c34a155ba1c460d7f
minerguy341 <jthuey2@gmail.com>
2016-03-22 20:00:27 -0400

    Added files via upload
    Reference class- holds Mod ID, Name, and Version number

---
+++ lunariush/Reference.java
@@ -0,0 +1,8 @@
+package lunariush;
+
+public class Reference
+	{
+		public static final String MOD_ID = "tf";
+		public static final String MOD_NAME = "Thaumic Ferremancy";
+		public static final String VERSION = "1.0";
+	}
---
+++ lunariush/ThaumicFerremancy.java
@@ -0,0 +1,30 @@
+package lunariush;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+public class ThaumicFerremancy
+	{
+		@EventHandler
+		public void preInit(FMLPreInitializationEvent event)
+			{
+			
+			}
+		
+		@EventHandler
+		public void Init(FMLInitializationEvent event)
+			{
+		
+			}
+		
+		@EventHandler
+		public void postInit(FMLPostInitializationEvent event)
+			{
+		
+			}
+		
+	}
